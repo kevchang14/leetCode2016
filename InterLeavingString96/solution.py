79b2c2b527893be1e5249a5b7546fe4528b19fc4
kevchang14 <kwchang007@gmail.com>
2016-02-05 12:25:33 -0500

    DP solution

--- InterLeavingString96/solution.py
+++ InterLeavingString96/solution.py
@@ -7,16 +7,47 @@ class Solution(object):
         :rtype: bool
         """
 
-        s1Index = 0
-        s2Index = 0
-        for i, l in enumerate(s3):
-            if s1Index < len(s1) and s2Index < len(s2) and s1[s1Index] == l and s2[s2Index] == l:
-                return self.isInterleave(s1[s1Index+1:], s2[s2Index:], s3[i+1:]) or \
-                        self.isInterleave(s1[s1Index:], s2[s2Index+1:], s3[i+1:])
-            elif s1Index < len(s1) and s1[s1Index] == l:
-                s1Index += 1
-            elif s2Index < len(s2) and s2[s2Index] == l:
-                s2Index += 1
+        s1 += " "
+        s2 += " "
+        s3 += " "
+        results = [["" for j in range(0, len(s2) + 1)] for x in range(0, len(s1) + 1)]
+
+        results = self.insertNewLetter(s1, s2, s3, 0, 0, results)
+
+        j = 0
+        for i in range(1, len(s1)):
+            if results[i - 1][j] is not None and results[i][j] is not None:
+                results = self.insertNewLetter(s1, s2, s3, i, j, results)
             else:
-                return False
-        return True
+                results[i][j] = None
+
+        i=0
+        for j in range(1, len(s2)):
+            if results[i][j - 1] is not None and results[i][j] is not None:
+                results = self.insertNewLetter(s1, s2, s3, i, j, results)
+            else:
+                results[i][j] = None
+
+        for i in range(1, len(s1)):
+            for j in range(1, len(s2)):
+                if results[i - 1][j] is not None or results[i][j - 1] is not None:
+                    results = self.insertNewLetter(s1, s2, s3, i, j, results)
+                else:
+                    results[i][j] = None
+        return results[len(s1) - 1][len(s2) - 1] is not None
+
+
+    def insertNewLetter(self, s1, s2, s3, i, j, results):
+        if results[i][j] is None:
+            return results
+        elif s1[i] == s3[i + j] and s2[j] == s3[i + j]:
+            results[i][j] = s1[i]
+        elif s1[i] == s3[i + j]:
+            results[i][j] = s1[i]
+            results[i][j + 1] = None
+        elif s2[j] == s3[i + j]:
+            results[i][j] = s2[j]
+            results[i + 1][j] = None
+        else:
+            results[i][j] = None
+        return results
